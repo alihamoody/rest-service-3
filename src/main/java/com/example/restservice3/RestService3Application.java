45ef48f897f814272f1ac0395bc6b1d551c9473d
Ali Hamoody <alih@dev6.com>
2020-06-22 10:58:02 -0400

    Initial Commit

---
+++ src/main/java/com/example/restservice3/RestService3Application.java
@@ -0,0 +1,13 @@
+package com.example.restservice3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestService3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestService3Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/restservice3/RestService3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.restservice3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestService3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
