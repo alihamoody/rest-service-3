c013e8a490442101317ff8ea56cdc73b2e14cbed
Ali Hamoody <alih@dev6.com>
2020-06-22 11:55:47 -0400

    Added /health controller

---
+++ src/main/java/com/example/restservice3/controllers/HealthRestController.java
@@ -0,0 +1,19 @@
+package com.example.restservice3.controllers;
+
+import java.time.Instant;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthRestController {
+
+  @GetMapping(value="/health")
+  public String getMethodName() {
+      return "Service is up and running. " + Instant.now().toString();
+  }
+
+
+  
+
+}
