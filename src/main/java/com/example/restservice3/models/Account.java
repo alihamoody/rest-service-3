7cb5ddf989921ef883a58abb1de41577d0869893
Ali Hamoody <alih@dev6.com>
2020-06-26 12:44:39 -0400

    Added lombok

--- src/main/java/com/example/restservice3/models/Account.java
+++ src/main/java/com/example/restservice3/models/Account.java
@@ -1,10 +1,13 @@
 package com.example.restservice3.models;
 
+import lombok.Data;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
+@Data
 @Entity
 public class Account {
 
@@ -27,23 +30,5 @@ public class Account {
     this.description = newData.getDescription();
   }
 
-  public Long getId() {
-    return id;
-  }
-  public void setId(Long id) {
-    this.id = id;
-  }
-  public String getTitle() {
-    return title;
-  }
-  public void setTitle(String title) {
-    this.title = title;
-  }
-  public String getDescription() {
-    return description;
-  }
-  public void setDescription(String description) {
-    this.description = description;
-  }
-  
+
 }
