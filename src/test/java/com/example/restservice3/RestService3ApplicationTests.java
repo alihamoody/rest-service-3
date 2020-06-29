56abf0a3593560143369ff90b95ab106c29620fa
Ali Hamoody <alih@dev6.com>
2020-06-29 15:39:36 -0400

    Added getAccountTest

--- src/test/java/com/example/restservice3/RestService3ApplicationTests.java
+++ src/test/java/com/example/restservice3/RestService3ApplicationTests.java
@@ -2,9 +2,13 @@ package com.example.restservice3;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import com.example.restservice3.models.Account;
+
 import static org.hamcrest.Matchers.containsString;
 
 
@@ -12,14 +16,16 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 @SpringBootTest
 @AutoConfigureMockMvc
 class RestService3ApplicationTests {
 
 	@Autowired
-private MockMvc mockMvc;
+	private MockMvc mockMvc;
 
 	@Test
 	void contextLoads() {
@@ -38,4 +44,25 @@ private MockMvc mockMvc;
 		.andExpect(content().string(containsString("Hello World!")));
 	}
 
+	@Test
+	public void getAllAccountsTest() throws Exception {
+		mockMvc.perform(
+			get("/accounts")
+			.accept(MediaType.APPLICATION_JSON)
+			)
+		.andDo(print())
+		.andExpect(status().isOk())
+		.andExpect(MockMvcResultMatchers.jsonPath("$[*]").exists())
+		.andExpect(MockMvcResultMatchers.jsonPath("$[*].id").isNotEmpty())
+		.andExpect(MockMvcResultMatchers.jsonPath("$[0].title").value("Title2"));
+	}
+
+	// @Test
+	// public void shouldCreateNewAccount() throws Exception {
+	// 	this.mockMvc.perform(post("/accounts").content(MediaType.APPLICATION_JSON_VALUE).content(
+	// 		"{\"personId\" : 1,\"title\": \"Title Test\",\"description\": \"Desc Test\"}"))
+	// 	.andDo(print())
+	// 	.andExpect(status().isOk())
+	// 	.andExpect(content().string(containsString("Saved")));
+	// }
 }
