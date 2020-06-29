de9823f261d0a5ced7eec15d268a36e2ad02fd96
Ali Hamoody <alih@dev6.com>
2020-06-29 12:28:39 -0400

    Added test step to jenkinsfile and one test assertion

--- Jenkinsfile
+++ Jenkinsfile
@@ -21,6 +21,7 @@ pipeline {
         stage('Test') {
             steps {
                 echo 'Testing..'
+                sh './mvnw test'
             }
         }
         stage('Deploy') {
--- src/test/java/com/example/restservice3/RestService3ApplicationTests.java
+++ src/test/java/com/example/restservice3/RestService3ApplicationTests.java
@@ -1,5 +1,7 @@
 package com.example.restservice3;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -9,5 +11,11 @@ class RestService3ApplicationTests {
 	@Test
 	void contextLoads() {
 	}
+
+	@Test
+	void test0() {
+		assertEquals(2, 1+1, "1+1 failed!");
+	}
+
 
 }
