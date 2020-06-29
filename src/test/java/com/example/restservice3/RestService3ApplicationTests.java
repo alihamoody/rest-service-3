9105b77933ce0cece99504aab7f1dec2d890c9c1
Ali Hamoody <alih@dev6.com>
2020-06-29 13:17:34 -0400

    Added MockMvc test

--- src/test/java/com/example/restservice3/RestService3ApplicationTests.java
+++ src/test/java/com/example/restservice3/RestService3ApplicationTests.java
@@ -1,12 +1,26 @@
 package com.example.restservice3;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.contains;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.hamcrest.Matchers.containsString;
+
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
 
 @SpringBootTest
+@AutoConfigureMockMvc
 class RestService3ApplicationTests {
+
+	@Autowired
+private MockMvc mockMvc;
 
 	@Test
 	void contextLoads() {
@@ -17,5 +31,12 @@ class RestService3ApplicationTests {
 		assertEquals(2, 1+1, "1+1 failed!");
 	}
 
+	@Test
+	public void shouldReturnDefaultMessage() throws Exception {
+		this.mockMvc.perform(get("/hello"))
+		.andDo(print())
+		.andExpect(status().isOk())
+		.andExpect(content().string(containsString("Hello World!")));
+	}
 
 }
